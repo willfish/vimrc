e99d03f3a89e22e83a7d802ebb90ae4223346aae
William Fish <william.michael.fish@gmail.com>
2018-03-30 12:58:22 +0100

    Adds gitigore file

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #! /usr/bin/env bash
 RUNTIME=~/.vim_runtime 
+
 git clone https://github.com/willfish/vimrc $RUNTIME
+
 cat > ~/.vimrc <<VIMRC 
 source $RUNTIME/vimrcs/vundle.vim
 source $RUNTIME/vimrcs/basic.vim
 source $RUNTIME/vimrcs/filetypes.vim
 source $RUNTIME/vimrcs/plugins_config.vim
 VIMRC
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "Open VIM and run the following:"
+echo
+echo ":PluginInstall"
