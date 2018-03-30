2a9c5437038a553eb1a4fa6c29c69477cdf42a62
William Fish <william.michael.fish@gmail.com>
2018-03-30 13:11:47 +0100

    Updates setup.sh documentation

--- setup.sh
+++ setup.sh
@@ -12,6 +12,18 @@ VIMRC
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo "Open VIM and run the following:"
+echo "Install and configure homebrew:"
+echo "  /usr/bin/ruby -e \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\""
+echo "brew install node"
 echo
-echo ":PluginInstall"
+echo "Install and configure:"
+echo "  rbenv, pyenv, exenv "
+echo
+echo "Install the following:"
+echo "  gem install haml-lint erubi brakeman rails_best_practices rubocop"
+echo "  pip install vint"
+echo "  npm install -g eslint"
+echo
+echo "Open nvim and run the following:"
+echo "  :PluginInstall"
+echo "  :checkhealth"
