9d45dde6f8745364df75add2f33e2a63fbbd4543
William Fish <william.michael.fish@gmail.com>
2019-06-09 00:06:44 +0100

    Support vim and neovim out of the box

--- setup.sh
+++ setup.sh
@@ -1,13 +1,22 @@
 #! /usr/bin/env bash
 
-INIT_VIM=~/.config/nvim/init.vim
+INIT_NVIM=~/.config/nvim/init.vim
+INIT_VIM=~/.vimrc
 RUNTIME=~/.vim_runtime
 
 ln -sf $"(pwd)" $RUNTIME
+ln -sf $"(pwd)"/init.vim $INIT_NVIM
 ln -sf $"(pwd)"/init.vim $INIT_VIM
 
 PLUG_GIT=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-PLUG_VIM=~/.local/share/nvim/site/autoload/plug.vim
+PLUG_NVIM=~/.local/share/nvim/site/autoload/plug.vim
+PLUG_VIM=~/.vim/autoload/plug.vim
+
+if [ -f "$PLUG_NVIM" ]; then
+	echo "$PLUG_NVIM exists"
+else
+	curl -fLo $PLUG_NVIM --create-dirs $PLUG_GIT
+fi
 
 if [ -f "$PLUG_VIM" ]; then
 	echo "$PLUG_VIM exists"
