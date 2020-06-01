a68e5f6432b78ce4de89ea087b68f2408191b7b9
William Fish <william.michael.fish@gmail.com>
2020-06-01 22:07:23 +0100

    Fix weird ln issue

--- setup.zsh
+++ setup.zsh
@@ -3,27 +3,33 @@
 INIT_NVIM=~/.config/nvim/init.vim
 INIT_VIM=~/.vimrc
 
-mkdir -p ~/.config/nvim
-mkdir -p ~/.vim/autoload/
-mkdir -p ~/.local/share/nvim/site/autoload/
-
-ln -sf $(pwd)/init.vim $INIT_NVIM
-ln -sf $(pwd)/init.vim $INIT_VIM
-
-COC_NVIM=~/.config/nvim/coc-settings.json
-COC_VIM=~/.vim/coc-settings.json
-
-ln -sf $(pwd)/coc-settings.json $COC_NVIM
-ln -sf $(pwd)/coc-settings.json $COC_VIM
-
 PLUG_GIT=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 PLUG_INSTALL_DIR=~/.plug
 PLUG_NVIM=~/.local/share/nvim/site/autoload/plug.vim
 PLUG_VIM=~/.vim/autoload/plug.vim
 
+COC_NVIM=~/.config/nvim/coc-settings.json
+COC_VIM=~/.vim/coc-settings.json
+
+mkdir -p ~/.config/nvim
+mkdir -p ~/.vim/autoload/
+mkdir -p ~/.local/share/nvim/site/autoload/
+
 curl -fLso $PLUG_INSTALL_DIR --create-dirs $PLUG_GIT
+
+ln -sf $(pwd)/init.vim $INIT_NVIM
+ln -sf $(pwd)/init.vim $INIT_VIM
+
+ln -sf $(pwd)/coc-settings.json $COC_NVIM
+ln -sf $(pwd)/coc-settings.json $COC_VIM
 
 ln -sf $PLUG_INSTALL_DIR $PLUG_NVIM
 ln -sf $PLUG_INSTALL_DIR $PLUG_VIM
+
+# Strangely, ln -sf symlinks the source to a directory inside of itself
+# if the file in the current directory does not exist
+#
+# This could be a bug in ln
+[[ -f ~/.vim_runtime ]] && rm -f ~/.vim_runtime
 
 ln -sf $(pwd) ~/.vim_runtime
