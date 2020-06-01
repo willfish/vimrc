5cb84b0be6d0fd8389a2882dcfab7821cca496bc
William Fish <william.michael.fish@gmail.com>
2020-06-01 21:58:09 +0100

    Move to zsh

--- setup.zsh
+++ setup.zsh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#! /usr/bin/env zsh
 
 INIT_NVIM=~/.config/nvim/init.vim
 INIT_VIM=~/.vimrc
@@ -13,8 +13,8 @@ ln -sf $(pwd)/init.vim $INIT_VIM
 COC_NVIM=~/.config/nvim/coc-settings.json
 COC_VIM=~/.vim/coc-settings.json
 
-[[ -f $COC_NVIM ]] || ln -sf $(pwd)/coc-settings.json $COC_NVIM
-[[ -f $COC_VIM ]] || ln -sf $(pwd)/coc-settings.json $COC_VIM
+ln -sf $(pwd)/coc-settings.json $COC_NVIM
+ln -sf $(pwd)/coc-settings.json $COC_VIM
 
 PLUG_GIT=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 PLUG_INSTALL_DIR=~/.plug
@@ -23,6 +23,7 @@ PLUG_VIM=~/.vim/autoload/plug.vim
 
 curl -fLso $PLUG_INSTALL_DIR --create-dirs $PLUG_GIT
 
-ln -sf $(pwd) $HOME/.vim_runtime
 ln -sf $PLUG_INSTALL_DIR $PLUG_NVIM
 ln -sf $PLUG_INSTALL_DIR $PLUG_VIM
+
+ln -sf $(pwd) ~/.vim_runtime
