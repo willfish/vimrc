0354fdab17bcf5e8ae9ca0b26efaa523de80eec1
William Fish <william.michael.fish@gmail.com>
2021-01-11 14:40:29 +0000

    Adds some convenience global printers

--- lua/init.lua
+++ lua/init.lua
@@ -1,14 +1,21 @@
-vim.api.nvim_command("set runtimepath^=~/.vim runtimepath+=~/.vim/after runtimepath+=~/.config/nvim")
-vim.api.nvim_command("let &packpath = &runtimepath")
+R = function(name)
+    RELOAD(name)
+    return require(name)
+end
+
+P = function(v)
+    print(vim.inspect(v))
+    return v
+end
 
 -- Basic configuration
-require('opts') -- Global, Window and Buffer configuration options
-require('maps') -- Normal, Insert, Visual, Terminal mode mappings
+require("opts") -- Global, Window and Buffer configuration options
+require("maps") -- Normal, Insert, Visual, Terminal mode mappings
 
 -- Advanced configuration
-require('plugins') -- Pack plugins
-require('advanced') -- Pack plugins configuration
+require("plugins") -- Pack plugins
+require("advanced") -- Pack plugins configuration
 
 -- Need to be migrated to lua <3
-vim.cmd('source ~/.vim_runtime/vimrcs/unmigratable.vim')
-vim.cmd('source ~/.vim_runtime/vimrcs/advanced.vim')
+vim.cmd("source ~/.vim_runtime/vimrcs/unmigratable.vim")
+vim.cmd("source ~/.vim_runtime/vimrcs/advanced.vim")
